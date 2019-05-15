e80ffeb579bba5dae63a5e7057262ce01c6f488e
yangfubing <1928881525@qq.com>
2019-05-15 11:21:09 +0800

    update
    Signed-off-by: yangfubing <1928881525@qq.com>

--- roseeureka/src/main/java/cn/burningmyself/roseeureka/RoseeurekaApplication.java
+++ roseeureka/src/main/java/cn/burningmyself/roseeureka/RoseeurekaApplication.java
@@ -2,10 +2,12 @@ package cn.burningmyself.roseeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
+@EnableEurekaClient
 public class RoseeurekaApplication {
 
     public static void main(String[] args) {
--- rosezuul/src/main/java/com/burningmyself/rosezuul/RosezuulApplication.java
+++ rosezuul/src/main/java/com/burningmyself/rosezuul/RosezuulApplication.java
@@ -2,10 +2,12 @@ package com.burningmyself.rosezuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableEurekaClient
 public class RosezuulApplication {
 
     public static void main(String[] args) {
