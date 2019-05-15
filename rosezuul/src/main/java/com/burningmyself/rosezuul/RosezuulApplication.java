3f2285eaa710f30096b9b9e9da8825e0dbfe7947
yangfubing <1928881525@qq.com>
2019-05-15 15:56:45 +0800

    update
    Signed-off-by: yangfubing <1928881525@qq.com>

--- rosezuul/src/main/java/com/burningmyself/rosezuul/RosezuulApplication.java
+++ rosezuul/src/main/java/com/burningmyself/rosezuul/RosezuulApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableEurekaClient
 public class RosezuulApplication {
 
     public static void main(String[] args) {
